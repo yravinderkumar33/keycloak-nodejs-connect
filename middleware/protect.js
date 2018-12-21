12942b8051da0a60b01a7fd9ac4377d38a18047e
harishkumar gangula <harishg@ilimi.in>
2018-12-21 19:21:18 +0530

    Issue #SB-9197 fix: update url

--- middleware/protect.js
+++ middleware/protect.js
@@ -27,7 +27,7 @@ function forceLogin (keycloak, request, response) {
   let redirectUrl = protocol + '://' + host + (port === '' ? '' : ':' + port) + (request.originalUrl || request.url) + (hasQuery ? '&' : '?') + 'auth_callback=1';
 
   if (request.session) {
-    request.session.auth_redirect_uri = redirectUrl;
+    request.session.auth_redirect_uri = redirectUrl + "&version=1";
   }
 
   let uuid = UUID();
